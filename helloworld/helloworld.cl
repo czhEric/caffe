fb8f93e0e71eebcec23e03050c01705a58bfd1d1
chenzhonghua <chenzhonghua@sensetime.com>
2018-10-10 21:17:41 +0800

    add code

--- helloworld/helloworld.cl
+++ helloworld/helloworld.cl
@@ -4,3 +4,10 @@ __kernel void helloworld(__global double* in, __global double* out)
 	out[num] = in[num] / 2.4 *(in[num]/6) ;
 }
 
+__kernel void hello_kernel(__global const float *a,
+                            __global const float *b,
+                            __global const float *result)
+{
+    int gid = get_global_id(0);
+    result[gid] = a[gid] + b[gid];
+}
--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -8,7 +8,8 @@
 #include <string>
 #include <fstream>
 using namespace std;
-
+#if 0
+//sample 1
 int main(int argc, char* argv[])
 {
     cl_int    status;
@@ -85,3 +86,114 @@ int main(int argc, char* argv[])
     }
     return 0;
 }
+#else
+int main(int argc, char** argv)
+{
+    cl_context context = 0;
+    cl_command_queue commandQueue = 0;
+    cl_program program;
+    cl_device_id device = 0;
+    cl_kernel kernel = 0;
+    cl_mem memObjects[3] = {0,0,0};
+    cl_int errNum;
+
+    //Create an OpenCL context on first available platform
+    context = CreateContext();
+    if(context == NULL)
+    {
+        cerr << "Failed to create OpenCL context." << endl;
+        return 1;
+    }
+
+    //Create a command-queue on the first device available on the created context
+    commandQueue = CreateCommandQueue(context, &device);
+    if(commandQueue == NULL)
+    {
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return -1;
+    }
+
+    //Create OpenCL program from helloworld.cl kernel source
+    program = CreateProgram(context, device, "helloworld.cl");
+    if(program == NULL)
+    {
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return -1;
+    }
+
+    //Create OpenCL kernel
+    kernel = clCreateKernel(program, "hello_kernel", NULL);
+    if(kernel == NULL)
+    {
+        cerr << "Failed to create kernel" << endl;
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return -1;
+    }
+
+    //Create memory objects that will be used as arguments to kernel.
+    //First Create host memory arrays that will be used to store the arguments to the kernel
+    #define ARRAY_SIZE 100
+    float result[ARRAY_SIZE];
+    float a[ARRAY_SIZE];
+    float b[ARRAY_SIZE];
+    for(int i = 0; i < ARRAY_SIZE; i++)
+    {
+        a[i] = i;
+        b[i] = i * 2;
+    }
+
+    if(!CreateMemObjects(context, memObjects, a, b))
+    {
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return -1;
+    }
+
+    //Set the kernel arguments(result, a, b)
+    errNum = clSetKernelArg(kernel, 0, sizeof(cl_mem), &memObjects[0]); //a
+    errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &memObjects[1]);//b
+    errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &memObjects[2]);//result
+    if(errNum != CL_SUCCESS)
+    {
+        cerr << "Error settint kernel arguments." << endl;
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return -1;
+    }
+
+    size_t globalWorkSize[1] = {ARRAY_SIZE};
+    size_t localWorkSize[1] = {1};
+
+    //Queue the kernal up for execution across the array
+    errNum = clEnqueueNDRangeKernel(commandQueue, kernel, 1, NULL,
+             globalWorkSize, localWorkSize, 0,
+                NULL, NULL);
+    if(errNum != CL_SUCCESS)
+    {
+        cerr << "Error queuing kernel for execution." << endl;
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return -1;
+    }
+
+    //Read the output buffer back to the Host
+    errNum = clEnqueueReadBuffer(commandQueue, memObjects[2], CL_TRUE/*blocking_read*/,
+                 0, ARRAY_SIZE * sizeof(float), result, 0, NULL, NULL);
+    if(errNum != CL_SUCCESS)
+    {
+        cerr << "Error reading result buffer." << endl;
+        Cleanup(context, commandQueue, program, kernel, memObjects);
+        return -1;
+    }
+
+    //Output the result buffer
+    for(int i = 0; i < ARRAY_SIZE; i++)
+    {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    cout << "Executed program successfully." << endl;
+    Cleanup(context, commandQueue, program, kernel, memObjects);
+    return 0;
+}
+#endif
+
+
+
